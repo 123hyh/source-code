be73aef193e0d3e010b6c8142ed310dc4541d990
huangyuhui <310515005@qq.com>
2020-04-04 01:06:34 +0800

    .

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Javascript Api 源码（基于 typescript）
 
+###### 开始
+
+`tsc --watch`
+
 1. [generator](https://github.com/123hyh/source-code/blob/master/generator.ts "generator") 原理
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generator_1 = require("./generator");
-generator_1.generator();
-//# sourceMappingURL=index.js.map
